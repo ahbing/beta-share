48dd4e8556bf736047b2dbf88906b8fcf71ab7e2
念树 <givemearainbowlife@gmail.com>
2015-05-12 12:20:40 +0800

    change to online

--- models/connect.js
+++ models/connect.js
@@ -1,5 +1,6 @@
 
 var mongoose = require('mongoose');
-module.exports =  mongoose.connect('mongodb://localhost/share');
+// 本地调试
+// module.exports =  mongoose.connect('mongodb://localhost/share');
 
-// module.exports =  mongoose.connect('mongodb://ahbing:ahbing@ds045511.mongolab.com:45511/share');
+module.exports =  mongoose.connect('mongodb://ahbing:ahbing@ds045511.mongolab.com:45511/share');
